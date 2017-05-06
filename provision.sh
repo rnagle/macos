52061575b2531d11759cd2391d77e077b9e596fb
Ryan Nagle <rmnagle@gmail.com>
2017-05-06 18:33:28 -0500

    headless install of xcode cmd line tools

--- provision.sh
+++ provision.sh
@@ -6,22 +6,24 @@ echo "You will be asked for your password."
 echo "* -----"
 echo ""
 
-function deps_check {
-  # Check for pip and install if necessary
-  pip_check=$(pip --version 2> /dev/null)
-
-  if [[ "$pip_check" == "" ]]
-  then
-    echo "Installing pip."
-    sudo easy_install pip
-    sudo pip install ansible
-  fi
+function xcode_install {
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD";
 }
 
 function provision {
+  echo "Installing Xcode Command Line Tools"
+  xcode_install
+  echo "Installing pip."
+  sudo easy_install pip
+  sudo pip install ansible
   echo "Running playbook."
   ansible-playbook -K -i inventory -v main.yml
 }
 
-deps_check
 provision
